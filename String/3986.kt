acc89335a37699af28092e2007f13dfa85dbafe8
kimdoyun98 <rladlsxo1250@gmail.com>
2025-01-07 10:31:33 +0900

    백준 - 좋은 단어

---
+++ String/3986.kt
@@ -0,0 +1,22 @@
+package String
+
+fun goodWord(){
+    var result = 0
+    repeat(readln().toInt()){
+        val word = readln()
+
+        val string = buildString {
+            word.forEach {
+                if(this.isNotBlank() && this.last() == it){
+                    setLength(this.length - 1)
+                }
+                else {
+                    append(it)
+                }
+            }
+        }
+
+        if(string.isBlank()) result++
+    }
+    print(result)
+}
