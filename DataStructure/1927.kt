c7e0dd4415a5f2de6aa7a7938155dbba07c47732
kimdoyun98 <rladlsxo1250@gmail.com>
2025-02-18 06:22:57 +0900

    백준 - 최소 힙 (S2)

---
+++ DataStructure/1927.kt
@@ -0,0 +1,17 @@
+package DataStructure
+
+import java.util.*
+
+fun _1927() {
+    val arr = PriorityQueue<Int>()
+
+    repeat(readln().toInt()) {
+        val input = readln().toInt()
+        if (input == 0) {
+            if (arr.isEmpty()) println(0)
+            else println(arr.poll())
+        } else {
+            arr.add(input)
+        }
+    }
+}
